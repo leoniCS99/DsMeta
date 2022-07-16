0abf1c4546f6481e8d195def665ed222ad204eda
Leoni Cesar <leonecezar99@gmail.com>
2022-07-16 16:43:51 -0300

    inclusao Sales Card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
-import NotificationButton from './components/NotificationButton';
+import SalesCard from './components/SalesCard';
+import Header from './components/Header';
 
 function App() {
- return(
-  <>
-  <h1>olá Mundo!</h1>
-  <NotificationButton/>
-  </>
- )
+    return (
+        <>
+            <Header />
+            <main>
+                <section id="sales">
+                    <div className="dsmeta-container">
+                        <SalesCard />
+                    </div>
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default App;
